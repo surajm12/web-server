ffbcf7ad11d269080bbe16747a88bae85569066c
Suraj Mistry <abcvolvo12@gmail.com>
2015-11-11 06:30:04 +0530

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,11 @@
+var middleware = {
+  requireAuthentication: function(req,res,next) {
+    console.log('private route hit!');
+  },
+    logger: function (req, res, next) {
+       console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+       next();    
+    }
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -2,15 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var middleware = {
-  requireAuthentication: function(req,res,next) {
-    console.log('private route hit!');
-  },
-    logger: function (req, res, next) {
-       console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-       next();    
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
